806ff1fbd824658208171bd2ee023c47e7151e0a
ryuhanda <r.handa@everyleaf.com>
2022-02-04 10:14:35 +0900

    新規登録時に名前を設定できるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_update_parameters, if: :devise_controller?
+  before_action :configure_permitted_signup_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
+  def configure_permitted_update_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def configure_permitted_signup_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
